d9b04d6b13824c3564beaff9e3e9d88b15f6eb8b
Olanrewaju Toheeb Olawale <toheebolawale019@gmail.com>
2025-11-03 05:22:54 +0100

    Refactor using class

--- script.js
+++ script.js
@@ -21,95 +21,107 @@ close.addEventListener('click', () =>{
 
 
 
-const myLibrary = [];
 
-function Book(name, author, numberOfPages, read, id){
-    this.name = name;
-    this.author = author;
-    this.numberOfPages = numberOfPages;
-    this.read = read;
-    this.id = id;
+
+class Book{
+    constructor(name, author, numberOfPages, read, id){
+        this.name = name;
+        this.author = author;
+        this.numberOfPages = numberOfPages;
+        this.read = read;
+        this.id = id;
+        
+
+    }
 }
 
+class Library{
 
-function addBookToLibrary(book){
+    constructor(){
+        this.books = [];
+    }
 
-    myLibrary.push(book);
-    displayBook();
+    update(){
+        display.textContent = "";
+        
+        this.books.forEach(book => {
+
+            const card = document.createElement('div');
+            card.classList.add('card');
+
+            const titleHeader = document.createElement("div");
+            titleHeader.textContent = `Name Of Book: ${book.name}`;
+
+            const authorName = document.createElement("div");
+            authorName.textContent = `Author: ${book.author}`;
+
+            const numberOfPages = document.createElement('div');
+            numberOfPages.textContent = `Pages: ${book.numberOfPages}`;
+
+            const readButton = document.createElement('input');
+            readButton.type = "checkbox";
+            readButton.id = `read${book.id}`;
+            readButton.checked = false;
+            readButton.style.cssText = `
+                width: 24px;
+                height:24px;
+                accent-color: red;
+                cursor:pointer;
+            `;
+
+            const labelForReadButton = document.createElement('label');
+            labelForReadButton.htmlFor = `read${book.id}`;
+            labelForReadButton.textContent = "Have You Read This Book?";
+            
+
+            const deleteButton = document.createElement('button');
+            deleteButton.textContent = "Delete Book";
+            deleteButton.classList.add("delete");
+            deleteButton.dataset.id = book.id;
+            deleteButton.style.cssText = `
+                padding: 5px 10px ;
+                border-radius: 10px;
+                font-size : 15px;
+                font-family: "Oswald", sans-serif
+                border: none;
+                cursor: pointer;
+            
+            `;
+
+
+            const container = document.createElement('div');
+            container.appendChild(labelForReadButton);
+            container.appendChild(readButton);
+            container.style.cssText = `
+                display: flex;
+                gap : 10px;
+                align-items: center;
+                justify-content:center;
+            `
+
+            card.appendChild(titleHeader);
+            card.appendChild(authorName);
+            card.appendChild(numberOfPages);
+            card.appendChild(container);
+            card.appendChild(deleteButton);
+            display.appendChild(card);
+
+
+        })
+    }
+
+    addBook(book){
+        this.books.push(book);
+        this.update();
+    }
+
+    removeBook(id){
+        this.books = this.books.filter(book => book.id != id);
+        this.update();
+    }
 }
 
-function displayBook(){
-
-    display.textContent = "";
-    
-    myLibrary.forEach(book => {
-
-        
-        const card = document.createElement('div');
-        card.classList.add('card');
-
-        const titleHeader = document.createElement("div");
-        titleHeader.textContent = `Name Of Book: ${book.name}`;
-
-        const authorName = document.createElement("div");
-        authorName.textContent = `Author: ${book.author}`;
-
-        const numberOfPages = document.createElement('div');
-        numberOfPages.textContent = `Pages: ${book.numberOfPages}`;
-
-        const readButton = document.createElement('input');
-        readButton.type = "checkbox";
-        readButton.id = "bookRead";
-        readButton.checked = false;
-        readButton.style.cssText = `
-            width: 24px;
-            height:24px;
-            accent-color: red;
-            cursor:pointer;
-        `;
-
-        const labelForReadButton = document.createElement('label');
-        labelForReadButton.htmlFor = 'bookRead';
-        labelForReadButton.textContent = "Have You Read This Book?";
-         
-
-        const deleteButton = document.createElement('button');
-        deleteButton.textContent = "Delete Book";
-        deleteButton.classList.add("delete");
-        deleteButton.dataset.id = book.id;
-        deleteButton.style.cssText = `
-            padding: 5px 10px ;
-            border-radius: 10px;
-            font-size : 15px;
-            font-family: "Oswald", sans-serif
-            border: none;
-            cursor: pointer;
-        
-        `;
-
-        const container = document.createElement('div');
-        container.appendChild(labelForReadButton);
-        container.appendChild(readButton);
-        container.style.cssText = `
-            display: flex;
-            gap : 10px;
-            align-items: center;
-            justify-content:center;
-        `
-
-        card.appendChild(titleHeader);
-        card.appendChild(authorName);
-        card.appendChild(numberOfPages);
-        card.appendChild(container);
-        card.appendChild(deleteButton);
-        display.appendChild(card);
-        
-        
-    })
-}
-
-
-
+const library = new Library();
 
 submit.addEventListener("click", (e)=>{
 
@@ -118,12 +130,16 @@ submit.addEventListener("click", (e)=>{
         return;
     }
     e.preventDefault(); 
-    let book = new Book();
-    book.author = bookAuthor.value;
-    book.name = bookTitle.value;
-    book.numberOfPages = numberOfpages.value;
-    book.id = crypto.randomUUID();
-    addBookToLibrary(book);
+    const book = new Book(
+        bookTitle.value,
+        bookAuthor.value,
+        numberOfpages.value,
+        false,
+        crypto.randomUUID()
+    );
+   
+    library.addBook(book);
+
     bookAuthor.value = "";
     bookTitle.value = "";
     numberOfpages.value = "";
@@ -135,10 +151,6 @@ display.addEventListener('click', (e)=> {
 
     if(e.target.classList.contains('delete')){
         const bookId = e.target.dataset.id;
-        const newLibrary = myLibrary.filter(book => book.id != bookId);
-        myLibrary.length = 0;
-        myLibrary.push(...newLibrary);
-
-        displayBook();
+        library.removeBook(bookId);
     }
 })
